0b37feac303e964953bc0725d4e7df9be1cf606f
brenden-sullivan <bsulli13@u.rochester.edu>
2017-10-09 21:10:40 -0700

    added port and ip to server

--- server.js
+++ server.js
@@ -2489,8 +2489,8 @@ function logErrors(err) {
 
 /**************************************************/
 //start server
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 http.listen(server_port, server_ip_address, function(){
   console.log("Listening on " + server_ip_address + ", server_port " + server_port);
